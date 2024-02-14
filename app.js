51ebccab1c8c229e618b22125db23ba73224d98f
Thejvanth <thejvanth08@gmail.com>
2024-02-14 19:47:29 +0530

    code add-post functionality

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const app = express();
 
+// list of blogs
+const blogs = [];
 
 // configuring the app
 app.set("view engine", "ejs");
 
 app.use(express.static("./public"));
 
+// to use form data parse
+// without using this - req.body will be undefined
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {blogs: blogs});
 });
 
 app.get("/compose-page", (req, res) => {
   res.render("compose");
 });
 
-app.post("add-post")
+app.post("/add-blog", (req, res) => {
+  // name for input field == variable or property key of that value
+  const { title, content} = req.body;
+
+  // shorthand
+  const blog = {title, content}; 
+  blogs.push(blog);
+  res.redirect("/");
+});
 
 
 app.listen(3000, () => {
   console.log("server is running at port 3000...");
-})
+});
